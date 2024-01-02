c204c863403bc10f764ee74d126e7c929132c5b3
joejoe <joejoeelephant@gmail.com>
2024-01-02 14:03:50 +0800

    blogPreviewACrd text-size

--- src/app/blogPreviewCard/page.tsx
+++ src/app/blogPreviewCard/page.tsx
@@ -4,28 +4,30 @@ import Image from 'next/image'
 
 export default function page() {
   return (
-    <div className="card w-full rounded-3xl overflow-hidden p-5 bg-white border-2 border-gray-800">
-      <div className="rounded-xl overflow-hidden">
-        <Image src={'/assets/images/illustration-article.svg'} width={336} height={201} alt='illustration' className='w-full' priority></Image>
-      </div>
-      <div className='card-tag inline-block mt-4 font-bold rounded-md py-1 px-3 text-sm'>
-        Learning
-      </div>
-      <p className='mt-4 text-sm font-semibold'>
-        Lorem, ipsum dolor.
-      </p>
-      <h3 className='card-h3 text-xl font-extrabold mt-4 cursor-pointer'>
-        HTML & CSS foundations
-      </h3>
-      <p className="card-desc text-sm mt-4 leading-relaxed">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, non iste. Optio.
-      </p>
-      <div className='flex items-center gap-4 mt-6'>
-        <div className='rounded-full'>
-          <Image src={'/assets/images/image-avatar.webp'} alt='avatar' width={64} height={65} className='w-full'></Image>
+    <div className='card-container'>
+      <div className="card w-full rounded-3xl overflow-hidden p-5 bg-white border-2 border-gray-800">
+        <div className="rounded-xl overflow-hidden">
+          <Image src={'/assets/images/illustration-article.svg'} width={336} height={201} alt='illustration' className='w-full' priority></Image>
         </div>
-        <div className='font-extrabold'>
-          Greg Hooper
+        <div className='card-tag inline-block mt-4 font-bold rounded-md py-1 px-3'>
+          Learning
+        </div>
+        <p className='mt-4 font-semibold'>
+          Lorem, ipsum dolor.
+        </p>
+        <h3 className='card-text-lg font-extrabold mt-4 cursor-pointer'>
+          HTML & CSS foundations
+        </h3>
+        <p className="card-desc mt-4 leading-relaxed">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, non iste. Optio.
+        </p>
+        <div className='flex items-center gap-4 mt-6'>
+          <div className='rounded-full'>
+            <Image src={'/assets/images/image-avatar.webp'} alt='avatar' width={64} height={65} className='w-full'></Image>
+          </div>
+          <div className='font-extrabold'>
+            Greg Hooper
+          </div>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
 export default function Home() {
   return (
-    <main>
-      Page
+    <main className='h-screen flex justify-center items-center bg-blue-500'>
+      <Link href={'/blogPreviewCard'} className='text-white text-xl font-bold underline'>
+        blogPreviewCard
+      </Link>
     </main>
   )
 }
