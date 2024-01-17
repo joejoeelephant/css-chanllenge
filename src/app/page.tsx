2096ed321729b718ff51094ee0aff68b86014111
joejoe <joejoeelephant@gmail.com>
2024-01-17 15:01:11 +0800

    page link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,27 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className='min-h-screen flex flex-col gap-4 justify-center items-center bg-blue-500'>
-      <Link href={'/blogPreviewCard'} className='text-white text-xl font-bold underline'>
+      <a href={'/blogPreviewCard'} className='text-white text-xl font-bold underline'>
         blogPreviewCard
-      </Link>
-      <Link href={'/faq-accordion-main'} className='text-white text-xl font-bold underline'>
+      </a>
+      <a href={'/faq-accordion-main'} className='text-white text-xl font-bold underline'>
         faq-accordion-main
-      </Link>
-      <Link href={'/newsletter-sign-up'} className='text-white text-xl font-bold underline'>
+      </a>
+      <a href={'/newsletter-sign-up'} className='text-white text-xl font-bold underline'>
         newsletter-sign-up
-      </Link>
-      <Link href={'/multi-step-form'} className='text-white text-xl font-bold underline'>
+      </a>
+      <a href={'/multi-step-form'} className='text-white text-xl font-bold underline'>
         multi-step-form
-      </Link>
-      <Link href={'/interactive-comments'} className='text-white text-xl font-bold underline'>
+      </a>
+      <a href={'/interactive-comments'} className='text-white text-xl font-bold underline'>
         interactive-comments
-      </Link>
-      <Link href={'/ecommerce-product-page'} className='text-white text-xl font-bold underline'>
+      </a>
+      <a href={'/ecommerce-product-page'} className='text-white text-xl font-bold underline'>
         ecommerce-product-page
-      </Link>
-      <Link href={'/todo-app-main'} className='text-white text-xl font-bold underline'>
+      </a>
+      <a href={'/todo-app-main'} className='text-white text-xl font-bold underline'>
         todo-app-main
-      </Link>
+      </a>
     </main>
   )
 }
